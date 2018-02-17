425907ff3e28141e047e5a207163a59e5a7ece14
Briant Oliveira <briantmoliveira@gmail.com>
2018-02-17 12:26:41 -0700

    added some changes

---
+++ server.development.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path');
+var webpack = require('webpack');
+var config = require('./webpack.config.development');
+var app = express();
+var compiler = webpack(config);
+// var React = require('react');
+// require('node-jsx').install({
+//   harmony: true,
+//   extension: '.jsx'
+// });
+// var Contest = React.createFactory(require('./src/pages/contest/page')); // returns a function
+
+// var db = require('./models');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('beststuff', 'postgres', null, { dialect: 'postgres' });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+var middleware = require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: 'src',
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  }
+});
+
+
+
+app.use(middleware);
+app.use(require('webpack-hot-middleware')(compiler, {
+  log: console.log
+}));
+
+app.get('*', function response(req, res) {
+  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+  res.end();
+});
+
+app.use(express.static(path.join(__dirname, '/dist')))
+app.use(express.static('public'))
+
+//Import controllers
+
+app.listen(config._hotPort, 'localhost', function (err) {
+  if (err) {
+    console.log(err)
+  }
+  // db.sequelize.sync();
+  console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', config._hotPort, config._hotPort)
+})
---
+++ server.production.js
@@ -0,0 +1,20 @@
+var path = require('path')
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 8000));
+
+app.use(express.static(path.join(__dirname, '/dist')))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
+});
+
+//Import Controllers
+
+app.listen(app.get('port'), function (err) {
+  if (err) {
+    console.log(err)
+  }
+  console.info('==> I\'m Listening on port %s.', app.get('port'));
+});
